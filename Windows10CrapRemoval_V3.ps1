defef20edbe0828a43fd3c6127ac17c7930a68e5
Tomasz Wycinka <49656666+Llamaclysm@users.noreply.github.com>
2019-07-25 18:40:13 +0200

    Update Windows10CrapRemoval_V3.ps1
    updated list & tweaks

--- Windows10CrapRemoval_V3.ps1
+++ Windows10CrapRemoval_V3.ps1
@@ -1,6 +1,46 @@
-$bloatware = @(
-    "*xbox*"
+<#
+
+A command to get an app that should be removed:
+    Get-AppxPackage -allusers | Select Name, PackageFullName
+
+Not on the list (various reasons):
     "*office*"
+
+#>
+$bloatware = @(
+    "*Bing*"
+    "*office*"
+    "*Facebook*"
+    "*Linkedin*"
+    "*xbox*"
+    "*twitter*"
+    "*zune*"
+    "*solitaire*"
+    "*netflix*"
+    "*skype*"
+    "*Sketchable*"
+    "*yourphone*"
+    "*freshpaint*"
+    "*MixedReality*"
+    "*Microsoft3DViewer*"
+    "*print3D*"
+    "*windowscommunicationsapps*"
+    "*Microsoft.People*"
+    "*Microsoft.Messaging*"
+    "*Microsoft.Windows.Photos*"
+    "*Microsoft.YourPhone*"
+    "*Microsoft.MsixPackagingTool*"
+    "*dropbox*"
+    "*CandyCrush*"
+    "*windowsphone*"
+    "*AutodeskSketchBook*"
+    "*Xboxapp*"
+    "Microsoft.XboxIdentityProvider"
+    "Microsoft.Xbox.TCUI"
+    "Microsoft.XboxSpeechToTextOverlay"
+    "Microsoft.OneConnect"
+    "Microsoft.XboxGamingOverlay"
+    "DellInc.DellDigitalDelivery" 
 )
 
 foreach ($bloat in $bloatware) {
