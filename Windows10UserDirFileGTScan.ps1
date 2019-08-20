574cf945d8d09d0eec36a6bcac884d3a0014014d
Tomasz Wycinka <49656666+Llamaclysm@users.noreply.github.com>
2019-08-20 20:13:02 +0200

    Create Windows10UserDirFileGTScan.ps1

---
+++ Windows10UserDirFileGTScan.ps1
@@ -0,0 +1,16 @@
+<#
+
+A simple script to list files within a user directory with size greater than X
+
+#>
+
+
+echo "Pick a size: "
+$S = Read-Host 
+echo "Select a user: "
+$U = Read-Host 
+$D = "C:\Users\"
+$P = "$D$U"
+#echo "Path to scan: $P"
+
+Get-ChildItem -Path $P -Recurse | Where-Object {$_.Length -gt $S}
