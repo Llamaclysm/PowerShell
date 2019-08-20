404608edb17655043cee40319a7e5bbf6edb3267
Tomasz Wycinka <49656666+Llamaclysm@users.noreply.github.com>
2019-08-20 16:57:43 +0200

    Create Windows10ComputerRename.ps1

---
+++ Windows10ComputerRename.ps1
@@ -0,0 +1,28 @@
+<#
+
+A simple script to rename a computer to meet a company naming standard;
+
+Variables used:
+    $pc - a variable to be used as a laptop / pc modifier
+    $vm - a variable to be used as a virtual machine modifier
+    $dvm - a variable to be used as a dev virtual machine modifier
+    $s - serial number 
+
+#> 
+
+clear
+
+$pc = "PC"
+$vm = "vm-i-"
+$dvm = "dev-vm-i-"
+
+$s = Get-WmiObject win32_bios | Select-Object -ExpandProperty SerialNumber
+
+echo "Local System Serial number : $s"
+echo "Local System New Name : $pc$s"
+
+# already in AD 
+Rename-Computer -NewName "$pc$s"
+
+# NOT in AD 
+#Rename-Computer -NewName "$pc$s" -LocalCredential WS\Administrator -PassThru
