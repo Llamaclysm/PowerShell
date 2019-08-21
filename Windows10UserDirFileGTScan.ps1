4a939f97f27d0003570f6d2e643f6b00b63d167c
Tomasz Wycinka <49656666+Llamaclysm@users.noreply.github.com>
2019-08-21 15:37:41 +0200

    reworked + menu added

--- Windows10UserDirFileGTScan.ps1
+++ Windows10UserDirFileGTScan.ps1
@@ -1,20 +1,34 @@
 <#
-A simple script to list files within a given directory with size greater than X
+A simple script to list files within a given directory with size greater than x
 #>
 
+# MENU
+Write-Host "1 - With static HOME directory: $HOME"
+Write-Host "2 - With customized HOME directory"
+Write-Host "3 - With fully customized directory"
+[int]$MenuItem = Read-Host -Prompt 'Select a proper option'
 
-$S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
-# static home dir
-#$P = $HOME
-# set a customized dir @C:\Users\XXX\
-$U = Read-Host -Prompt 'Select a username'
-#$U = $env:USERNAME
-$D = "C:\Users\"
-$P = "$D$U"
-# set a fully customized dir
-#$P = Read-Host -Prompt 'Select a path'
-# debug section only - just ignore 
-#echo "Path to scan: $P"
+if($MenuItem -eq 1){
+    Write-Host "1 - selected"
+    $P = $HOME
+    $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
+}
+elseif($MenuItem -eq 2){
+    Write-Host "2 - selected"
+    $U = Read-Host -Prompt 'Select a username'
+    $D = "C:\Users\"
+    $P = "$D$U"
+    $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
+}
+elseif($MenuItem -eq 3){
+    Write-Host "3 - selected"
+    $P = Read-Host -Prompt 'Select a path to scan'
+    $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
+}
+else {
+    Write-Host "Wrong selection"
+}
+
 Write-Host "Scanning for files greater than $S in a directory: $P"
 
 $LargeFiles = Get-ChildItem -Path $P -Recurse | Where-Object {$_.Length -gt $S} 
@@ -22,4 +36,4 @@ $LargeFilesCount = $LargeFiles | Measure-Object | Select-Object -ExpandProperty 
 foreach ($i in $LargeFiles) {
     Write-Host "$i"
 }
-Write-Host "$LargeFilesCount files total."
+Write-Host "W sumie $LargeFilesCount plikow"
