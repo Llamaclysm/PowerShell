8a0059754dca3fa1e9f188f39796de1db6963e29
Tomasz Wycinka <49656666+Llamaclysm@users.noreply.github.com>
2019-08-21 15:47:47 +0200

    Update Windows10UserDirFileGTScan.ps1

--- Windows10UserDirFileGTScan.ps1
+++ Windows10UserDirFileGTScan.ps1
@@ -8,20 +8,21 @@ Write-Host "2 - With customized HOME directory"
 Write-Host "3 - With fully customized directory"
 [int]$MenuItem = Read-Host -Prompt 'Select a proper option'
 
+# LOGIC
 if($MenuItem -eq 1){
-    Write-Host "1 - selected"
+    Write-Host "Scanning with static HOME directory: $HOME"
     $P = $HOME
     $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
 }
 elseif($MenuItem -eq 2){
-    Write-Host "2 - selected"
+    Write-Host "Scanning with customized HOME directory"
     $U = Read-Host -Prompt 'Select a username'
     $D = "C:\Users\"
     $P = "$D$U"
     $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
 }
 elseif($MenuItem -eq 3){
-    Write-Host "3 - selected"
+    Write-Host "Scanning with fully customized directory"
     $P = Read-Host -Prompt 'Select a path to scan'
     $S = Read-Host -Prompt 'Pick a size to search for (greater than ; in MB)'
 }
@@ -31,9 +32,10 @@ else {
 
 Write-Host "Scanning for files greater than $S in a directory: $P"
 
+# MAGIC
 $LargeFiles = Get-ChildItem -Path $P -Recurse | Where-Object {$_.Length -gt $S} 
 $LargeFilesCount = $LargeFiles | Measure-Object | Select-Object -ExpandProperty Count
 foreach ($i in $LargeFiles) {
     Write-Host "$i"
 }
-Write-Host "W sumie $LargeFilesCount plikow"
+Write-Host "$LargeFilesCount files total."
